f9fb0ad1ddacc996953b9cebc921d3601cf3ef92
JenaBiswaranjan <jenabiswa6372@gmail.com>
2024-10-30 13:23:48 +0530

    djnjdn

---
+++ src/main/java/in/mvc/SpringMvcFormValidationApplication.java
@@ -0,0 +1,13 @@
+package in.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFormValidationApplication.class, args);
+	}
+
+}
